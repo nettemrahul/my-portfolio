2fd342123a57d24980012ea8efb70413c63520de
nettemrahul <rahul.nettem@gmail.com>
2025-04-22 13:01:39 +0530

    commit new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
               <a className="nav-link" href="#projects">Projects</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/resume.pdf" download>Resume</a>
+            <a className="nav-link" href="/Resume.pdf" download>Resume</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#contact">Contact</a>
