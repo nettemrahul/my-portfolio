14e1a61ce4433ca02e983003ace3a2737d1b6cc8
nettemrahul <rahul.nettem@gmail.com>
2025-04-22 14:36:07 +0530

    changed code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand fw-bold" href="#about">Nettem Rahul Naidu</a>
+        <a className="navbar-brand" href="#hero">Portfolio</a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#projects">Projects</a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="/resume.pdf" download>Resume</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">Contact</a>
-            </li>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
+            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+            <li className="nav-item"><a className="nav-link" href="/resume.pdf" download>Resume</a></li>
           </ul>
         </div>
       </div>
